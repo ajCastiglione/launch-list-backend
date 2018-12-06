3e5c44810d24c6e332211c210795ff9b15d87832
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-12-06 11:49:24 -0500

    updated accepted headers

--- server/server.js
+++ server/server.js
@@ -13,10 +13,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
 
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, x-auth, Content-Type, Post-Type"
-  );
+  res.header("Access-Control-Allow-Headers", "Origin, x-auth, Content-Type");
   next();
 });
 
