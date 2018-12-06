12132f19ae02b59980732accf96f13d5a19bbc93
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-12-06 11:49:00 -0500

    fixed deprecation warning for new mongoose version

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose.promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true }
+);
 
 module.exports = { mongoose };
