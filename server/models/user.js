caf8f22c550c0ec75ad2308d03c4619a15039341
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-12-06 20:39:44 -0500

    updated user model with logging in and auth methods

--- server/models/user.js
+++ server/models/user.js
@@ -21,18 +21,117 @@ let UserSchema = new mongoose.Schema({
     required: true,
     minlength: 6
   },
+  role: {
+    type: String,
+    required: true
+  },
   credentials: [
     {
-      token: {
+      access: {
         type: String,
         required: true
       },
-      role: {
+      token: {
         type: String,
         required: true
       }
     }
   ]
+});
+
+// Attached to the schema
+UserSchema.methods.generateAuthToken = function() {
+  let user = this;
+  let access = "auth";
+  let token = jwt
+    .sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET)
+    .toString();
+
+  user.credentials = user.credentials.concat([{ access, token }]);
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+// Attached to the schema
+UserSchema.methods.removeCredentials = function() {
+  let user = this;
+  user.credentials = [];
+  return user;
+};
+
+// This is attached to the Model
+UserSchema.statics.findByToken = function(token) {
+  let User = this;
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    _id: decoded._id,
+    "credentials.token": token,
+    "credentials.access": "auth"
+  });
+};
+
+// This is attached to the Model
+UserSchema.statics.findByToken = function(token) {
+  let User = this;
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    _id: decoded._id,
+    "tokens.token": token,
+    "tokens.access": "auth"
+  });
+};
+
+// Attached to model - finds and authenticates existing user
+UserSchema.statics.findByCredentials = function(email, password) {
+  let User = this;
+
+  return User.findOne({ email }).then(user => {
+    if (!user) {
+      return Promise.reject("User does not exist");
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject("Authentication failed!");
+        }
+      });
+    });
+  });
+};
+
+// Encrypting password before saving to database - attached to schema user
+UserSchema.pre("save", function(next) {
+  let user = this;
+
+  if (user.isModified("password")) {
+    // Encrypt passowrd
+    bcrypt.genSalt(10, (err, salt) => {
+      // Set user password to hash
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        // use next() to continue
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", UserSchema);
