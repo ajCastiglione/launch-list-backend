19e07c9b202e05046e816268c7f30a58bac5f572
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-12-07 14:16:55 -0500

    update list model to include type of checklist

--- server/models/list.js
+++ server/models/list.js
@@ -7,6 +7,12 @@ const List = mongoose.model("List", {
     minlength: 1,
     trim: true
   },
+  type: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
   completed: {
     type: Boolean,
     default: false
