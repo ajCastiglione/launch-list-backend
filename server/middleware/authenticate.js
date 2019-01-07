85d38a8ead6795b8c61b25742c3fc266c058cd7d
ajCastiglione <antonio@minervawebdevelopment.com>
2019-01-07 15:16:22 -0500

    updated signup auth portion

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -19,18 +19,35 @@ const authenticate = (req, res, next) => {
 };
 
 const createUserAuth = (req, res, next) => {
-  let pw = CryptoJS.AES.decrypt(
-    req.header("superCommand"),
-    process.env.CRYPTO_SECRET
-  );
+  let token = req.header("x-auth");
 
-  if (pw === process.env.USER_CREATION_CODE) {
-    next();
+  if (token) {
+    User.findByToken(token)
+      .then(user => {
+        if (!user) {
+          return Promise.reject();
+        }
+        req.user = user;
+        req.token = token;
+        next();
+      })
+      .catch(e => {
+        res.status(401).send(e);
+      });
   } else {
-    res.status(401).send({
-      err:
-        "Code is incorrect. Please contact an administrator if you believe this was a mistake."
-    });
+    let pw = CryptoJS.AES.decrypt(
+      req.header("createCommand"),
+      process.env.CRYPTO_SECRET
+    ).toString(CryptoJS.enc.Utf8);
+    if (pw === process.env.USER_CREATION_CODE) {
+      req.user = "createCommand";
+      next();
+    } else {
+      res.status(401).send({
+        err:
+          "Code is incorrect. Please contact an administrator if you believe this was a mistake."
+      });
+    }
   }
 };
 
